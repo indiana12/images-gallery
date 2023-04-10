c4cbb83506d07c1a7d6ab0166152e69e0e4f2f0e
Lucky Prahalad <prahalad.luckyreddy@gmail.com>
2023-04-10 18:03:55 +0530

    initial header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header.js';
 
-function App() {
+const App =() => {
   return (
-    <div className="App">
-     <h1>Images Gallery</h1>
+    <div>
+      <Header title="Images Gallery"/>
     </div>
   );
 }
